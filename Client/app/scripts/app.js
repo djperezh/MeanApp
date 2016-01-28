423242cf194f73a3f4471c03e28189215f862d18
Domingo Perez <domperez@expedia.com>
2016-01-27 16:18:29 -0800

    updating files

--- Client/app/scripts/app.js
+++ Client/app/scripts/app.js
@@ -9,23 +9,36 @@
  * Main module of the application.
  */
 angular
-  .module('clientApp', [
-    'ngResource',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
+  .module('clientApp', ['ngRoute','restangular'])
+  .config(function ($routeProvider, RestangularProvider) {
+
+    // Set the base URL for Restangular.
+    RestangularProvider.setBaseUrl('http://localhost:3000');
+
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        controller: 'MainCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        controller: 'AboutCtrl'
+      })
+      .when('/movies', {
+        templateUrl: 'views/movies.html',
+        controller: 'MoviesCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .factory('MovieRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+      RestangularConfigurer.setRestangularFields({
+        id: '_id'
+      });
+    });
+  })
+  .factory('Movie', function(MovieRestangular) {
+    return MovieRestangular.service('movie');
   });
